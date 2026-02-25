595ee17c5aa50e6f62c23759132466a8e0d22619
dentrangtrihanoi <kyozet.seo@gmail.com>
2026-02-25 19:20:21 +0700

    SEO: add robots.txt and update next-sitemap config with correct domain and disallow rules

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,16 @@ module.exports = {
     siteUrl: process.env.SITE_URL || 'https://xinghiepcokhi.info',
     generateRobotsTxt: true,
     sitemapSize: 7000,
+    robotsTxtOptions: {
+        policies: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: ['/studio', '/api', '/studio/'],
+            },
+        ],
+        additionalSitemaps: [
+            `${process.env.SITE_URL || 'https://xinghiepcokhi.info'}/sitemap.xml`,
+        ],
+    },
 }
